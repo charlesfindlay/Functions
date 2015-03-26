59faf964dc0f700d79ed4b842a43501d80303e9f
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-03-26 12:24:27 -0400

    Added some methods on expense report

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -8,8 +8,53 @@ namespace Functions
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            string[] rules = { "entertainment", "towncar", "hardware" };
+            int[] caps = { 250, 500, 1000 };
+
+            Console.WriteLine("Please describe your expense:");
+            string description = Console.ReadLine();
+
+            Console.Write("Enter the amount of the expense: ");
+            decimal expense = decimal.Parse(Console.ReadLine());
+
+
+            // Probalby enclose in a do - while loop
+            int decisionLevel = 0;
+            int managerCap = caps[decisionLevel];
+            string managerRule = rules[decisionLevel];
+            
+
+
         }
+
+        
+        enum level
+        {
+            first,
+            second,
+            director,
+            ceo
+        }
+
+        public static bool CanApprove(decimal expense, int managerCap)
+        {
+            return (expense <= managerCap) ? true : false;
+        }
+
+        public static bool AgainstRules(string managerRule, string description)
+        {
+            foreach (string word in description.Split().ToArray())
+            {
+                if (word == managerRule)
+                    return false;
+            }
+            return true;
+          
+        }
+
+
     }
 }
